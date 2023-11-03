c20a7e6c8b33464b9a797a6cc344b0403f1ca885
Dana Tolman <61774862+dctii@users.noreply.github.com>
2023-11-03 07:54:45 -0700

    Update BuildingPrompt.java
    Update conditions for createIndustrialBuilding and createSkyscraper so user cannot put value above ceiling value

--- src/main/java/com/solvd/buildingco/interactive/BuildingPrompt.java
+++ src/main/java/com/solvd/buildingco/interactive/BuildingPrompt.java
@@ -53,14 +53,29 @@ public class BuildingPrompt {
 
 
     public static IndustrialBuilding createIndustrialBuilding(Scanner scanner) {
-        // ceiling value
+        // ceiling values
         final int maxSquareFootage = 100000;
+        final int maxFloors = 4;
 
-        System.out.print("Enter the square footage for the Industrial Building (up to " + maxSquareFootage + "): ");
-        int squareFootage = Math.min(scanner.nextInt(), maxSquareFootage);
+        int squareFootage = 0;
+        do {
+            System.out.print("Enter the square footage for the Industrial Building (up to " + maxSquareFootage + "): ");
+            squareFootage = scanner.nextInt();
+            if (squareFootage > maxSquareFootage || squareFootage < 1) {
+                System.out.println("Sorry, that's the ceiling value for square footage. Please " +
+                        "try again.");
+            }
+        } while (squareFootage > maxSquareFootage || squareFootage < 1);
 
-        System.out.print("Enter the number of floors for the Industrial Building (up to 4): ");
-        int numberOfFloors = Math.min(scanner.nextInt(), 4);
+        int numberOfFloors = 0;
+        do {
+            System.out.print("Enter the number of floors for the Industrial Building (up to " + maxFloors + "): ");
+            numberOfFloors = scanner.nextInt();
+            if (numberOfFloors > maxFloors || numberOfFloors < 1) {
+                System.out.println("Sorry, that's above the ceiling value for number of floors. " +
+                        "Please try again.");
+            }
+        } while (numberOfFloors > maxFloors || numberOfFloors < 1);
 
         return new IndustrialBuilding(squareFootage, numberOfFloors);
     }
@@ -70,11 +85,24 @@ public class BuildingPrompt {
         final int maxSquareFootagePerLevel = 50000;
         final int maxLevels = 100;
 
-        System.out.print("Enter the square footage per level for the Skyscraper (up to " + maxSquareFootagePerLevel + "): ");
-        int squareFootagePerLevel = Math.min(scanner.nextInt(), maxSquareFootagePerLevel);
+        int squareFootagePerLevel = 0;
+        do {
+            System.out.print("Enter the square footage per level for the Skyscraper (up to " + maxSquareFootagePerLevel + "): ");
+            squareFootagePerLevel = scanner.nextInt();
+            if (squareFootagePerLevel > maxSquareFootagePerLevel || squareFootagePerLevel < 1) {
+                System.out.println("Sorry, that's above the ceiling value for square footage. " +
+                        "PLease try again.");
+            }
+        } while (squareFootagePerLevel > maxSquareFootagePerLevel || squareFootagePerLevel < 1);
 
-        System.out.print("Enter the number of levels for the Skyscraper (up to " + maxLevels + "): ");
-        int levels = Math.min(scanner.nextInt(), maxLevels);
+        int levels = 0;
+        do {
+            System.out.print("Enter the number of levels for the Skyscraper (up to " + maxLevels + "): ");
+            levels = scanner.nextInt();
+            if (levels > maxLevels || levels < 1) {
+                System.out.println("Sorry, that's above the ceiling value for levels.Please try again.");
+            }
+        } while (levels > maxLevels || levels < 1);
 
         return new Skyscraper(squareFootagePerLevel, levels);
     }
