8fff3e5fbcff9634a857b14cf4359b67f5c60de8
dctii <danactolman@gmail.com>
2023-11-15 10:28:01 -0800

    minor grammar issue on second sentence

---
+++ src/main/java/com/solvd/buildingco/exception/BuildingTypeException.java
@@ -0,0 +1,6 @@
+package com.solvd.buildingco.exception;
+public class BuildingTypeException {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/solvd/buildingco/inventory/UnitMeasurement.java
@@ -0,0 +1,8 @@
+package com.solvd.buildingco.inventory;
+
+public final class UnitMeasurement {
+
+
+    private UnitMeasurement() {
+    }
+}
