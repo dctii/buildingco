541bd5da39d832077b0f1d769dc82af58bd06b45
dctii <danactolman@gmail.com>
2023-11-16 15:25:42 -0800

    change out ton measurement for unit

--- src/main/java/com/solvd/buildingco/inventory/ItemRepository.java
+++ src/main/java/com/solvd/buildingco/inventory/ItemRepository.java
@@ -20,8 +20,8 @@ public class ItemRepository {
         loadItem(ItemNames.PAINT, ItemPrices.PAINT, UnitMeasurement.GALLON);
         loadItem(ItemNames.PLUMBING_SUPPLIES, ItemPrices.PLUMBING_SUPPLIES, UnitMeasurement.UNIT);
         loadItem(ItemNames.ELECTRICAL_SUPPLIES_HOUSE, ItemPrices.ELECTRICAL_SUPPLIES_HOUSE, UnitMeasurement.UNIT);
-        loadItem(ItemNames.STEEL_BEAMS, ItemPrices.STEEL_BEAMS, UnitMeasurement.TON);
-        loadItem(ItemNames.STEEL_COLUMNS, ItemPrices.STEEL_COLUMNS, UnitMeasurement.TON);
+        loadItem(ItemNames.STEEL_BEAMS, ItemPrices.STEEL_BEAMS, UnitMeasurement.UNIT);
+        loadItem(ItemNames.STEEL_COLUMNS, ItemPrices.STEEL_COLUMNS, UnitMeasurement.UNIT);
         loadItem(ItemNames.CONCRETE_INDUSTRIAL, ItemPrices.CONCRETE_INDUSTRIAL, UnitMeasurement.SQUARE_FOOT);
         loadItem(ItemNames.GLASS_INDUSTRIAL, ItemPrices.GLASS_INDUSTRIAL, UnitMeasurement.SQUARE_FOOT);
         loadItem(ItemNames.ROOFING_INDUSTRIAL, ItemPrices.ROOFING_INDUSTRIAL, UnitMeasurement.SQUARE_FOOT);
@@ -30,7 +30,8 @@ public class ItemRepository {
                 UnitMeasurement.UNIT);
         loadItem(ItemNames.HVAC_SUPPLIES, ItemPrices.HVAC_SUPPLIES, UnitMeasurement.UNIT);
         loadItem(ItemNames.INTERIOR_FINISHING_MATERIALS, ItemPrices.INTERIOR_FINISHING_MATERIALS, UnitMeasurement.SQUARE_FOOT);
-        loadItem(ItemNames.STEEL_BEAMS_HIGH_GRADE, ItemPrices.STEEL_BEAMS_HIGH_GRADE, UnitMeasurement.TON);
+        loadItem(ItemNames.STEEL_BEAMS_HIGH_GRADE, ItemPrices.STEEL_BEAMS_HIGH_GRADE,
+                UnitMeasurement.UNIT);
         loadItem(ItemNames.CONCRETE_HIGH_GRADE, ItemPrices.CONCRETE_HIGH_GRADE, UnitMeasurement.SQUARE_FOOT);
         loadItem(ItemNames.GLASS_HIGH_GRADE_INDUSTRIAL, ItemPrices.GLASS_HIGH_GRADE_INDUSTRIAL, UnitMeasurement.SQUARE_FOOT);
 
