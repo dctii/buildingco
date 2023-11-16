c4695ceb4b0fd093aaafeaaf09e9e6973224136b
dctii <danactolman@gmail.com>
2023-11-16 15:18:28 -0800

    move orderItem loading logic from generateMaterialOrder()

--- src/main/java/com/solvd/buildingco/utilities/OrderUtils.java
+++ src/main/java/com/solvd/buildingco/utilities/OrderUtils.java
@@ -1,6 +1,20 @@
 package com.solvd.buildingco.utilities;
+
+import com.solvd.buildingco.finance.Order;
+import com.solvd.buildingco.finance.OrderItem;
+
+import java.util.ArrayList;
+
 public class OrderUtils {
-    public static void main(String[] args) {
-        
+    public static Order loadOrder (ArrayList<OrderItem> orderItems){
+        // initialize order
+        Order order = new Order();
+
+        // populate order with each item in orderItems
+        for (OrderItem item : orderItems) {
+            order.addOrderItem(item);
+        }
+
+        return order;
     }
 }
