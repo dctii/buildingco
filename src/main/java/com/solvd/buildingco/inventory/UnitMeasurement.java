c3f96bb375491c89a294a950579b22af955be3b2
dctii <danactolman@gmail.com>
2023-11-16 15:24:52 -0800

    remove ton measurement

--- src/main/java/com/solvd/buildingco/inventory/UnitMeasurement.java
+++ src/main/java/com/solvd/buildingco/inventory/UnitMeasurement.java
@@ -4,7 +4,7 @@ public final class UnitMeasurement {
 
     public final static String SQUARE_FOOT = "square foot";
     public final static String GALLON = "gallon";
-    public final static String TON = "ton";
+
     public final static String UNIT = "unit";
 
     private UnitMeasurement() {
