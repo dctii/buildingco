568571b6440717d48febdde56664c046ea67ca0b
dctii <danactolman@gmail.com>
2023-11-26 12:09:39 -0800

    move SEPARATOR_CHARS from WordCounterUtils to here; more detailed comments

--- src/main/java/com/solvd/buildingco/utilities/RegExpPatternConstants.java
+++ src/main/java/com/solvd/buildingco/utilities/RegExpPatternConstants.java
@@ -5,11 +5,13 @@ public final class RegExpPatternConstants {
         https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
         Contains reference for regexp constructs
 
-        Matches roman numerals `i` to `x`, 1 to 10, enclosed in parentheses. For example, "(iii)", "
-        (iv)", "
-        (X)", etc.
+        Matches roman numerals `i` to `x`, 1 to 10, enclosed in parentheses. For example, "(iii)",
+        "(iv)", "(X)", etc.
 
-        `\s*` - match any whitespace char
+        `X*` - zero or more times
+        `X{n, m} - at least n, but not more than m times
+        `X|Y` - either X or Y
+        `\s` - any whitespace char
         `(?i)` - case-insensitivity to deal with "III" and "iii" for roman numeral 3 for example
         `v?i{0,3}`
             - `v`'s appearance is optional
@@ -24,8 +26,24 @@ public final class RegExpPatternConstants {
 
     /*
         Matches digits enclosed in parentheses. For example, "(1)", "(2)", "(100)" etc.
+
+        `\d` - digit character [0-9]
+        `X+` - postfix to denote one or more times
      */
     public static final String NUMBERS_IN_PARENTHESES_PATTERN = "\\(\\d+\\)";
+
+    /*
+            Characters to split words by:
+                whitespace, tab, and newline
+                comma, semicolon, colon
+                sentence terminating signs
+                double quotes
+                parentheses, brackets, braces
+
+            `\t` - tab
+            `\n` - newline
+     */
+    public static final String SEPARATOR_CHARS = "[ ,.!?;:\"()\\[\\]{}<>\\t\\n]+";
 
     private RegExpPatternConstants() {
     }
