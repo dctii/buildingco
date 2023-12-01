975df50aaa6851717471560127b43ec6c52989bf
dctii <danactolman@gmail.com>
2023-11-30 22:05:59 -0800

    gather commonalities of constant Employee data from BuildingCoConstants and BuildingConstants and put into enum

--- src/main/java/com/solvd/buildingco/stakeholders/employees/Personnel.java
+++ src/main/java/com/solvd/buildingco/stakeholders/employees/Personnel.java
@@ -1,2 +1,47 @@
-package com.solvd.buildingco.stakeholders.employees;public enum Personnel {
+package com.solvd.buildingco.stakeholders.employees;
+
+public enum Personnel {
+    ARCHITECT(
+            "Architect",
+            "Architectural Design",
+            "architects@"
+    ),
+    CONSTRUCTION_WORKER(
+            "Construction Worker",
+            "Construction Work",
+            "workers@"
+    ),
+    ENGINEER(
+            "Engineer",
+            "Engineering Work",
+            "engineers@"
+    ),
+    PROJECT_MANAGER(
+            "Project Manager",
+            "Project Management",
+            "projectmanagers@"
+    );
+
+    private final String personnelType;
+    private final String defaultActivityDescription;
+    private final String departmentEmailHandle;
+
+    Personnel(String personnelType, String defaultActivityDescription,
+              String departmentEmailHandle) {
+        this.personnelType = personnelType;
+        this.defaultActivityDescription = defaultActivityDescription;
+        this.departmentEmailHandle = departmentEmailHandle;
+    }
+
+    public String getPersonnelType() {
+        return personnelType;
+    }
+
+    public String getDefaultActivityDescription() {
+        return defaultActivityDescription;
+    }
+
+    public String getDepartmentEmailHandle() {
+        return departmentEmailHandle;
+    }
 }
