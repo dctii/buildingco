a5115f6bcf5226071cb4d8b563295e397c7dad4e
dctii <danactolman@gmail.com>
2023-11-30 22:08:33 -0800

    remove default method

--- src/main/java/com/solvd/buildingco/utilities/ITax.java
+++ src/main/java/com/solvd/buildingco/utilities/ITax.java
@@ -1,20 +1,8 @@
 package com.solvd.buildingco.utilities;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 @FunctionalInterface
 public interface ITax {
     BigDecimal calculateTax(BigDecimal amount);
-    /*
-        "Can @FunctionalInterfaces have default methods?"
-        https://stackoverflow.com/questions/30165060/can-functionalinterfaces-have-default-methods
-    */
-    static BigDecimal calculateLosAngelesTax(BigDecimal amount) {
-        final double LA_TAX_RATE = 0.095;
-
-        return BigDecimalUtils
-                .multiply(amount, LA_TAX_RATE)
-                .setScale(2, RoundingMode.UP);
-    }
 }
