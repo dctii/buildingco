6164300d4c3aa6e2b5a6c36fc9759453454549d9
dctii <danactolman@gmail.com>
2023-12-01 17:12:32 -0800

    add new constants

--- src/main/java/com/solvd/buildingco/utilities/StringConstants.java
+++ src/main/java/com/solvd/buildingco/utilities/StringConstants.java
@@ -3,11 +3,15 @@ package com.solvd.buildingco.utilities;
 public final class StringConstants {
 
     public static final String EMPTY_STRING = "";
-    public static final String SINGLE_WHITESPACE_CHAR_STRING = " ";
-    public static final String MANUAL_EM_DASH_STRING = "--";
+    public static final String SINGLE_WHITESPACE = " ";
+    public static final String EM_DASH = "--";
     public static final String SINGLE_QUOTATION = "'";
-    public static final String CARRIAGE_RETURN_CHAR_STRING = "\r";
-    public static final String NEWLINE_CHAR_STRING = "\n";
+    public static final String CARRIAGE_RETURN = "\r";
+    public static final String NEWLINE = "\n";
+    public static final String PADDED_EQUALS_OPERATOR = " = ";
+    public static final String COMMA_DELIMITER = ", ";
+    public static final String COLON_DELIMITER = ": ";
+
 
     private StringConstants() {
         final String NO_CONSTANTS_INSTANTIATION_MESSAGE =
