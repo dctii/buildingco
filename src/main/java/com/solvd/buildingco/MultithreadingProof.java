6b3c3fd60b5857efc8529525dffe84860fb2f90e
dctii <danactolman@gmail.com>
2023-12-05 10:34:15 -0800

    add logger and placeholder for main for Multithreading main profile

--- src/main/java/com/solvd/buildingco/MultithreadingProof.java
+++ src/main/java/com/solvd/buildingco/MultithreadingProof.java
@@ -1,6 +1,12 @@
 package com.solvd.buildingco;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MultithreadingProof {
+    private static final Logger LOGGER = LogManager.getLogger(MultithreadingProof.class);
     public static void main(String[] args) {
-        
+        LOGGER.info("Multithreading logic will go here");
+
     }
 }
