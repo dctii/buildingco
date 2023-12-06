bcd4281acb7633d76afb8149cbeb48c599496b75
dctii <danactolman@gmail.com>
2023-12-05 22:14:27 -0800

    methods and their logic for new classes for multithreading

--- src/main/java/com/solvd/buildingco/multithreading/Connection.java
+++ src/main/java/com/solvd/buildingco/multithreading/Connection.java
@@ -1,7 +1,32 @@
 package com.solvd.buildingco.multithreading;
 
-public class Connection {
-    public static void main(String[] args) {
+import com.solvd.buildingco.utilities.StringFormatters;
 
+public class Connection {
+    private String name;
+
+    public Connection(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        Class<?> currClass = Connection.class;
+        String[] fieldNames = {
+                "name"
+        };
+
+        String fieldsString =
+                StringFormatters.buildFieldsString(this, fieldNames);
+
+        return StringFormatters.buildToString(currClass, fieldNames, fieldsString);
     }
 }
